f7929523a6b8cb1fb56d90264ab03962ded13106
sdeopuja <sdeopuja@in.ibm.com>
2024-09-12 22:55:58 +0530

    Add files via upload

---
+++ TestNG/Activities/Activity4Part2.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Activity4Part2 {
+    @Test
+    public void TestCase() {
+        System.out.println("I'm in the test case from DemoTwo Class");
+    }
+    
+}
---
+++ TestNG/Activities/Activity8.java
@@ -0,0 +1,5 @@
+package org.example;
+
+public class Activity8 {
+    
+}
