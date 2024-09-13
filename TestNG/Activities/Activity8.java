dae18b5d60fa233ce790f420a3473ecbcac18792
sdeopuja <sdeopuja@in.ibm.com>
2024-09-13 23:37:04 +0530

    Add files via upload

--- TestNG/Activities/Activity8.java
+++ TestNG/Activities/Activity8.java
@@ -1,5 +1,8 @@
-package org.example;
+package org.suiteExample;
 
 public class Activity8 {
+    public static void main(String[] args) {
+        System.out.println("Created a TestNg.xml file to run all the tests in parallel.");    
+    }
     
 }
